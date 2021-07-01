24ecc6aab8da74c1fdba1ac749bd5e6e5df890b9
Traian Gradinaru <traian.gradinaru@gmail.com>
2021-07-01 17:46:01 +0300

    Initial commit

---
+++ src/main/java/com/traian/newdemoproject/NewdemoprojectApplication.java
@@ -0,0 +1,13 @@
+package com.traian.newdemoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewdemoprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewdemoprojectApplication.class, args);
+	}
+
+}
